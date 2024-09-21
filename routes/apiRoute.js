3e8a61e6071896e97d980ae4e04583144ee68d3c
Ram Sahu <ramsahu321@gmail.com>
2024-09-22 02:16:25 +0530

    changes for accesing issue

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors")
 const { getItems, getInstaUsers, getChatbotData, getProductsData } = require("../controller/apiController");
 
 const router = express.Router();
--- server.js
+++ server.js
@@ -2,26 +2,24 @@ const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
 const rateLimiter = require("./middlewares/rateLimiter");
-const apiRoutes = require("./routes/apiRoute")
+const apiRoutes = require("./routes/apiRoute");
 
 const app = express();
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 5000;
 
 // middlewares ----
-app.use(cors())
-app.use(rateLimiter)
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow headers
+};
 
+app.use(cors(corsOptions));
+app.use(rateLimiter);
 
 // api routes ----
-app.use("/api/v2",apiRoutes)
+app.use("/api/v2", apiRoutes);
 
-// error handling ----
-app.get("/",(req,res)=>{
-    console.log("hii in console");
-    res.send("hii ram from backend");
-})
-
-app.listen(PORT, ()=>{
-    console.log(`server is connected at ${PORT}`.bgBlue);
-})
+app.listen(PORT, () => {
+  console.log(`server is connected at ${PORT}`.bgBlue);
+});
